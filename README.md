2d0149bf17326efb54d9a66119130a62b5d1cedc
dgleba <dgleba@users.noreply.github.com>
2019-02-23 09:28:24 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# mevn-Krunal-vuepouch445
-Created with CodeSandbox
+# mevnexample
+
+```
+source: https://github.com/KrunalLathiya/MEVNCRUDExample
+https://appdividend.com/2018/11/21/mevn-stack-tutorial-with-example-from-scratch/
+David Gleba 2019-02-22_Fri_07.09-AM
+```
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```
+npm run build
+```
+
+### Lints and fixes files
+
+```
+npm run lint
+```
---
+++ src/components/EditComponent.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <h1>Edit Post</h1>
+    <form @submit.prevent="updatePost">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Post Title: </label>
+            <input type="text" class="form-control" v-model="post.title" />
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Post Body: </label>
+            <textarea
+              class="form-control"
+              v-model="post.body"
+              rows="5"
+            ></textarea>
+          </div>
+        </div>
+      </div>
+      <br />
+      <div class="form-group">
+        <button class="btn btn-primary">Update</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        id: "",
+        _id: ""
+      }
+    };
+  },
+  created() {
+    // let uri = `http://localhost:4000/posts/edit/${this.$route.params.id}`;
+    // this.axios.get(uri).then(response => {
+    //   this.post = response.data;
+    // });
+  },
+  methods: {
+    updatePost() {
+      //   let uri = `http://localhost:4000/posts/update/${this.$route.params.id}`;
+      //   this.axios.post(uri, this.post).then(() => {
+      //     this.$router.push({ name: "posts" });
+      //   });
+    }
+  },
+  // Use the pouch property to configure the component to (reactively) read data from pouchdb.
+  pouch: {
+    // simple selector.  I put i here to prevent post undefined error?
+    mainlists: function() {
+      return {
+        //database: this.selectedDatabase, // you can pass a database string or a pouchdb instance
+        selector: {},
+        limit: this.resultsPerPage
+      };
+    }
+  }
+};
+</script>
